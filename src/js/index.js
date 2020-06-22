214994952411b1dcd8d8fb8b76eb7daefe25376e
Tomek Wilczak <tomek.wilczak@gmail.com>
2020-06-22 17:46:28 +0200

    Added js logic

--- src/js/index.js
+++ src/js/index.js
@@ -6,4 +6,24 @@ registerSW();
 
 /* place your code below */
 
-console.log("HELLO 🚀");
+let glassesQuant = 0;
+
+const waterGlass = document.querySelector(".water-glass");
+const addButton = document.querySelector(".button__add");
+const removeButton = document.querySelector(".button__remove");
+const glasses = document.querySelector(".water-glass__number");
+
+addButton.addEventListener("click", () => {
+  glassesQuant++;
+  glasses.innerHTML = `${glassesQuant}`;
+});
+
+removeButton.addEventListener("click", () => {
+  if (glassesQuant > 0) {
+    glassesQuant--;
+    glasses.innerHTML = `${glassesQuant}`;
+  } else {
+    glassesQuant = 0;
+    glasses.innerHTML = `${glassesQuant}`;
+  }
+});
