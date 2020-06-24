183639425093dac3906081d99c11ca619775b9c0
Tomek Wilczak <tomek.wilczak@gmail.com>
2020-06-24 19:29:30 +0200

    Fixed adding new date to localstorage (js)

--- src/js/index.js
+++ src/js/index.js
@@ -10,21 +10,25 @@ const addButton = document.querySelector(".button__add");
 const removeButton = document.querySelector(".button__remove");
 const waterGlasses = document.querySelector(".water-glass__counter");
 const key = new Date().toISOString().slice(0, 10);
-let glassesCount = localStorage.getItem(key);
+let glassesCount = "";
 
 // Checking if key already exists in localstorage
-if (key) {
+if (localStorage.getItem(key)) {
+  glassesCount = localStorage.getItem(key);
   waterGlasses.innerHTML = glassesCount;
 } else {
+  localStorage.setItem(key, 0);
   waterGlasses.innerHTML = "0";
+  console.log("Ustawione 0");
 }
 
 // Adding another glass of water
 addButton.addEventListener("click", (e) => {
   e.preventDefault();
   glassesCount++;
-  waterGlasses.innerHTML = `${glassesCount}`;
+  waterGlasses.innerHTML = glassesCount;
   localStorage.setItem(key, glassesCount);
+  console.log(`Ustawiono ${glassesCount}`);
 });
 
 // Removing a glass of water
@@ -32,11 +36,11 @@ removeButton.addEventListener("click", (e) => {
   if (glassesCount > 0) {
     e.preventDefault();
     glassesCount--;
-    waterGlasses.innerHTML = `${glassesCount}`;
+    waterGlasses.innerHTML = glassesCount;
     localStorage.setItem(key, glassesCount);
   } else {
     glassesCount = 0;
-    waterGlasses.innerHTML = `${glassesCount}`;
+    waterGlasses.innerHTML = glassesCount;
     e.preventDefault();
     localStorage.setItem(key, glassesCount);
   }
